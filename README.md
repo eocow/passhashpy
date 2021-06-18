bbca83ecab6b1e4953945414c15b7d838eaca04a
eocow <70074743+eocow@users.noreply.github.com>
2021-06-18 05:02:58 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# passhashpy
+# passhashpy Password Hash and Login System
+## Will input passwords into hash then store until finding matching hash inputed 
+User will input if they are logging in or registering through CLI promt
+It then takes their password input, creates a sha256 hash in a hex format then either appends that to the txt file or checks if the hash exits
+
+This was a fun project for me to learn and get comfortable with python and project development in general. Also want to play with hashing rather than just plain text
---
+++ hash.py
@@ -0,0 +1,46 @@
+import hashlib, sys, codecs
+
+loginqna = input("Are you logging in or registering?: ")
+if loginqna == "logging in" or "l":
+        logintof = True
+elif loginqna == "registering" or "r":
+        logintof = False 
+else:
+        print("Invalid Response")
+        sys.exit()
+
+usrinp = input("Enter your Password: ")
+
+usrinpd = usrinp.encode("utf-8")
+
+usrhash = hashlib.sha256()
+
+usrhash.update(usrinpd)
+
+temphash = usrhash.hexdigest()
+
+#print(temphash)
+#print(logintof)
+
+if logintof == True:
+        passwrd = open("/home/eo/vs-work/py/working-project-1/hashpassreg/passwrd.txt", "r")
+        flag = 0
+        index = 0
+        for line in passwrd:
+                index += 1 
+                if temphash in line:
+                        flag = 1
+                        break 
+        if flag == 1: 
+                print("Correct Password!")
+                passwrd.close
+                sys.exit
+        else:
+                print("Incorrect Password.")
+                passwrd.close()
+                sys.exit
+elif logintof == False:
+        passwrda = open("/home/eo/vs-work/py/working-project-1/hashpassreg/passwrd.txt", "a")
+        passwrda.write("\n" + temphash)
+        passwrda.close()
+        sys.exit()
